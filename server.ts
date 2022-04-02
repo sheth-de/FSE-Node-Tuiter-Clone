83224f61748933368d2825f3cf721ac33187850e
Devanshi Sheth <devanshisheth@Devanshis-MacBook-Pro.local>
2022-04-02 18:32:21 -0400

    added a4

--- server.ts
+++ server.ts
@@ -21,12 +21,13 @@ import mongoose from "mongoose";
 import GroupController from "./controllers/GroupController";
 const cors = require("cors");
 const session = require("express-session");
+require('dotenv').config()
 
 // build the connection string
 const PROTOCOL = "mongodb+srv";
 const DB_USERNAME = process.env.DB_USERNAME;
 const DB_PASSWORD = process.env.DB_PASSWORD;
-const HOST = "cluster0.m8jeh.mongodb.net";
+const HOST = "cluster0.lqk17.mongodb.net";
 const DB_NAME = "myFirstDatabase";
 const DB_QUERY = "retryWrites=true&w=majority";
 const connectionString = `${PROTOCOL}://${DB_USERNAME}:${DB_PASSWORD}@${HOST}/${DB_NAME}?${DB_QUERY}`;// connect to the database
